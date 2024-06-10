f4b0f778d9223d936a0a0f819057daa8d31ef0da
Hafidz Izzudin <hafidz.mudzakir@ruangguru.com>
2024-06-10 14:29:33 +0700

    chore(producer): isolate from unnecessary package

--- producer.php
+++ producer.php
@@ -1,8 +1,32 @@
 <?php
+require 'vendor/autoload.php';
 
+use Dotenv\Dotenv;
 use Src\Entity\Email;
+use Src\Module\EmailSender\EmailSenderPHPMailer;
+use Src\Repository\Email\EmailSQLRepository;
+use Src\Service\EmailService;
+use Src\System\DatabaseConnector;
+use Src\System\Mailer;
+use Src\System\RedisQueue;
 
-require 'bootstrap.php';
+// load env var
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// init system
+$dbConnection = (new DatabaseConnector())->getDbConnection();
+$phpMailer = (new Mailer())->getMailer();
+$queueContext = (new RedisQueue())->getContext();
+
+// init repo
+$emailRepository = new EmailSQLRepository($dbConnection);
+
+// init module 
+$emailSender = new EmailSenderPHPMailer($phpMailer, $queueContext);
+
+// init service
+$emailService = new EmailService($emailRepository, $emailSender);
 
 for ($i = 0; $i < 1; $i++) {
     $emailService->sendEmail(new Email("user_id_" . ($i + 1), true, 'hafidz.izzudin' . ($i + 1) . '@gmail.com', '<br>Nice adasdasd<br>', "payslip " . ($i + 1)));
